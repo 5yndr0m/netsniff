27e094fb33042c685094cbd7293cc0ee78bc12e5
5yndr0m <sdilanjana21@gmail.com>
2026-02-03 15:01:02 +0530

    feat: add a loop to linten to ethernet packets

--- netsniff.py
+++ netsniff.py
@@ -1,6 +1,13 @@
 import socket
 import struct
 import textwrap
+from ast import While
+
+
+# Return formatted MAC address (output -> XX:XX:XX:XX:XX:XX)
+def get_mac_addr(bytes_addr):
+    bytes_str = map("{:02x}".format, bytes_addr)
+    return ":".join(bytes_str).upper()
 
 
 # Unpack ethernet frame
@@ -14,7 +21,18 @@ def ethernet_frame_unpack(data):
     )
 
 
-# Return formatted MAC address (output -> XX:XX:XX:XX:XX:XX)
-def get_mac_addr(bytes_addr):
-    bytes_str = map("{:02x}".format, bytes_addr)
-    return ":".join(bytes_str).upper()
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame_unpack(raw_data)
+        print("\nEthernet Frame:")
+        print(
+            "Destination: {}, Source: {}, Protocol: {}".format(
+                dest_mac, src_mac, eth_proto
+            )
+        )
+
+
+main()
