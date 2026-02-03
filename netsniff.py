961b552bc6b5f5443b43934bb52f2b4253dbc696
5yndr0m <sdilanjana21@gmail.com>
2026-02-03 15:44:43 +0530

    feat: add func to unpack ipv4 packet and format ip addr

--- netsniff.py
+++ netsniff.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 import textwrap
-from ast import While
 
 
 # Return formatted MAC address (output -> XX:XX:XX:XX:XX:XX)
@@ -21,9 +20,32 @@ def ethernet_frame_unpack(data):
     )
 
 
+# Unpack IPv4 packet
+def ipv4_packet_unpack(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack("! 8x B B 2x 4s 4s", data[:20])
+    return (
+        version,
+        header_length,
+        ttl,
+        proto,
+        ipv4_format(src),
+        ipv4_format(target),
+        data[header_length:],
+    )
+
+
+# Format IPv4 address
+def ipv4_format(addr):
+    return ".".join(map(str, addr))
+
+
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
+    # Infinite loop to keep sniffing packets
     while True:
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame_unpack(raw_data)
